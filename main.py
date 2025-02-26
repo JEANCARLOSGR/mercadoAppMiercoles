f0780340851fe6c0166841e325b57ec3d88ce08e
INNVISAS <139381141+INNVISAS@users.noreply.github.com>
2025-02-26 10:24:02 -0500

    feat se crea menu de opciones en main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 #programa para gestion de productos de uuna lista de mercado 
 nombreUsuario=None
+
+#delarando variables 
+productos=[]
+producto={}
+
+#crear un menu de opciones 
+print("*** MerqueoAPP ***")
+print("1. Agregar Producto a tu lista de mercado")
+print("Presiona 5 para SALIR")
+
+opcion=100
+while opcion != 5:
+    opcion=int(input("Digita una opcion del menu: "))
+    
